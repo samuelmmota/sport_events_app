d3e9548ba36ac3e571f3a5e09ec1aedda4eeb494
Samuel Mota <samuelmmota@gmail.com>
2024-11-15 17:22:25 +0000

    Changed repository API fetch to use IO thread

--- app/src/main/java/s/m/mota/sporteventsapp/repository/SportsRepository.kt
+++ app/src/main/java/s/m/mota/sporteventsapp/repository/SportsRepository.kt
@@ -1,6 +1,8 @@
 package s.m.mota.sporteventsapp.repository
 
 import android.util.Log
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import s.m.mota.sporteventsapp.models.SportCategory
 import s.m.mota.sporteventsapp.retrofit.SportEventsApi
 
@@ -8,12 +10,14 @@ class SportsRepository(private val sportEventsApi: SportEventsApi) {
     private val TAG = "SportsRepository"
 
     suspend fun fetchSportCategories(): List<SportCategory> {
-        return try {
-            val result = sportEventsApi.getSportCategories() ?: emptyList()
-            result.filterNotNull()
-        } catch (e: Exception) {
-            Log.e(TAG, "Unexpected error. Fetch unsuccessful.", e)
-            emptyList<SportCategory>()
+        return withContext(Dispatchers.IO) {
+            try {
+                val result = sportEventsApi.getSportCategories() ?: emptyList()
+                result.filterNotNull()
+            } catch (e: Exception) {
+                Log.e(TAG, "Unexpected error. Fetch unsuccessful.", e)
+                emptyList<SportCategory>()
+            }
         }
     }
 }
